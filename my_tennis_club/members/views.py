153080253324884dcfeabf99b0f518483204bfe9
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-04 21:11:47 -0500

    Django Data From The Models

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -23,23 +23,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'cars': [
-      {
-        'brand': 'Ford',
-        'model': 'Mustang',
-        'year': '1964',
-      },
-      {
-        'brand': 'Ford',
-        'model': 'Bronco',
-        'year': '1970',
-      },
-      {
-        'brand': 'Volvo',
-        'model': 'P1800',
-        'year': '1964',
-      }]
-    }
-  return HttpResponse(template.render(context, request))  
+    'members': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
