eeb2f060996ad310b650d5d5aad1ff4eb0b5615e
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-04 20:51:33 -0500

    Django Template Tags

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -23,9 +23,8 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'mymembers': mymembers,
+    'greeting': 1,
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request))  
