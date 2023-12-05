cb478edfe73fd57564579660eecc08b8a63530d1
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-05 00:35:37 -0500

    Django Return Specific Rows

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -23,11 +23,12 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mydata = Member.objects.values_list('firstname')
+  mydata = Member.objects.filter(firstname='Emil').values()
   template = loader.get_template('template.html')
   context = {
     'mymembers': mydata,
   }
   return HttpResponse(template.render(context, request))
-    
+      
 
+      
